e4376074d157d41997d78eb0896ef3306941483c
XinYueStudio <775746081@qq.com>
2020-07-20 14:48:46 +0800

    Add files via upload
    动画进度控制脚本

---
+++ AnimationFrame.cs
@@ -0,0 +1,42 @@
+﻿ 
+ using UnityEngine;
+
+
+[RequireComponent(typeof(Animation))]
+public class AnimationFrame : MonoBehaviour
+{
+    Animation _animationTimeline;
+
+    public Animation animationTimeline
+    {
+        get
+        {
+            if (_animationTimeline == null)
+            {
+                _animationTimeline = GetComponent<Animation>();
+            }
+            return _animationTimeline;
+        }
+    }
+    public string AnimationName;
+
+    public void SetFrame(float frame)
+    {
+        animationTimeline[AnimationName].speed = 0;
+        animationTimeline[AnimationName].time = frame;
+        animationTimeline.Play(AnimationName);
+    }
+
+    [Range(0.0f, 1.0f)]
+    public float Frame = 0;
+    float mFrame = 0;
+    public void Update()
+    {
+        if (mFrame != Frame)
+        {
+            SetFrame(Frame);
+            mFrame = Frame;
+        }
+    }
+}
+ 
---
+++ PlayableDirectorFrame.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class PlayableDirectorFrame : MonoBehaviour
+{
+    PlayableDirector _mPlayableDirector;
+   
+    public PlayableDirector mPlayableDirector
+    {
+        get
+        {
+            if(_mPlayableDirector==null)
+            {
+                _mPlayableDirector = GetComponent<PlayableDirector>();
+            }
+            return _mPlayableDirector;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPlayableDirector.timeUpdateMode = DirectorUpdateMode.GameTime;
+    }
+
+
+    //大小根据帧数和mPlayableDirector 区间计算
+    [Range(0.0f, 1.0f)]
+    public float Frame = 0;
+    float mFrame = 0;
+
+ 
+
+    public void Update()
+    {
+        if (mFrame != Frame)
+        {
+            mPlayableDirector.time = Frame;
+            mPlayableDirector.RebuildGraph();
+            mPlayableDirector.Play();
+            mPlayableDirector.playableGraph.GetRootPlayable(0).SetSpeed(0);
+            mFrame = Frame;
+           
+        }
+    }
+
+    public void LateUpdate()
+    {
+        
+           
+            
+    }
+}
